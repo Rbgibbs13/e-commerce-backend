3d8e30046d1ea4bde85a5fdf337eb8eef5b7167b
unknown <rhett.gibbs@yahoo.com>
2024-03-10 11:58:06 -0700

    Models finished

---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: Tag,
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: Product,
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
